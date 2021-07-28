5a40e7ceaad4014edc6788caeb0e545246a45f3f
Vincent Ahn <jmahn97@hotmail.com>
2021-07-28 14:47:15 -0400

    Insert triangle cursor detection algorithms

--- src/scripts/elements/triangle.js
+++ src/scripts/elements/triangle.js
@@ -4,7 +4,7 @@ class Triangle extends Element{
   constructor(canvas){
     super(canvas);
     this.sideLength = 60;
-    this.altitudeR = this.sideLength * Math.sqrt(3) / 3;
+    this.altitudeR = this.sideLength / Math.sqrt(3);
     this.angle = 0; // degree
   }
 
@@ -55,6 +55,42 @@ class Triangle extends Element{
     this.draw();
 
     this.context.restore();
+  }
+
+  confirmInsideElement(x, y){
+    // Algorithm for calculating points of triangle after rotation in https://gamedev.stackexchange.com/questions/86755/how-to-calculate-corner-positions-marks-of-a-rotated-tilted-rectangle
+    // Incredible algorithm for detecting cursor in triangle found in http://totologic.blogspot.com/2014/01/accurate-point-in-triangle-test.html
+
+    let p1 = [this.posX - this.sideLength / 2, this.posY - this.altitudeR / 2];
+    let p2 = [this.posX, this.posY + this.altitudeR];
+    let p3 = [this.posX + this.sideLength / 2, this.posY - this.altitudeR / 2];
+
+    let tempP1 = [p1[0] - this.posX, p1[1] - this.posY];
+    let tempP2 = [p2[0] - this.posX, p2[1] - this.posY];
+    let tempP3 = [p3[0] - this.posX, p3[1] - this.posY];
+
+    p1[0] = tempP1[0] * Math.cos(this.angle * Math.PI / 180) - tempP1[1] * Math.sin(this.angle * Math.PI / 180) + this.posX;
+    p1[1] = tempP1[0] * Math.sin(this.angle * Math.PI / 180) + tempP1[1] * Math.cos(this.angle * Math.PI / 180) + this.posY;
+    p2[0] = tempP2[0] * Math.cos(this.angle * Math.PI / 180) - tempP2[1] * Math.sin(this.angle * Math.PI / 180) + this.posX;
+    p2[1] = tempP2[0] * Math.sin(this.angle * Math.PI / 180) + tempP2[1] * Math.cos(this.angle * Math.PI / 180) + this.posY;
+    p3[0] = tempP3[0] * Math.cos(this.angle * Math.PI / 180) - tempP3[1] * Math.sin(this.angle * Math.PI / 180) + this.posX;
+    p3[1] = tempP3[0] * Math.sin(this.angle * Math.PI / 180) + tempP3[1] * Math.cos(this.angle * Math.PI / 180) + this.posY;
+
+    let v1 = [p2[1] - p1[1], -p2[0] + p1[0]];
+    let v2 = [p3[1] - p2[1], -p3[0] + p2[0]];
+    let v3 = [p1[1] - p3[1], -p1[0] + p3[0]];
+    
+    let v1c = [x - p1[0], y - p1[1]];
+    let v2c = [x - p2[0], y - p2[1]];
+    let v3c = [x - p3[0], y - p3[1]];
+    
+    function scalarProduct(vec1, vec2){
+      return vec1[0] * vec2[0] + vec1[1] * vec2[1];
+    }
+
+    const calculation = 0 <= scalarProduct(v1, v1c) && 0 <= scalarProduct(v2, v2c) && 0 <= scalarProduct(v3, v3c)
+
+    return calculation;
   }
 };
 
