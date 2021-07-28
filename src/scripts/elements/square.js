0ea126104b9ef2491968995ca0adc088f3edfa3c
Vincent Ahn <jmahn97@hotmail.com>
2021-07-28 09:35:52 -0400

    Add square mouse detection
    Added algorithm to detect if mouse cursor on click is within square
    element.

--- src/scripts/elements/square.js
+++ src/scripts/elements/square.js
@@ -35,9 +35,9 @@ class Square extends Element{
     this.context.rotate(this.angle * Math.PI / 180);
     this.context.translate(-this.posX, -this.posY);
 
-    const topLeft = this.posX - this.sideLength / 2;
-    const topRight = this.posY - this.sideLength / 2;
-    this.context.fillRect(topLeft, topRight, this.sideLength, this.sideLength);
+    const topLeftX = this.posX - this.sideLength / 2;
+    const topLeftY = this.posY - this.sideLength / 2;
+    this.context.fillRect(topLeftX, topLeftY, this.sideLength, this.sideLength);
   }
   
   render(animating){
@@ -53,6 +53,44 @@ class Square extends Element{
     this.draw();
 
     this.context.restore();
+  }
+
+  confirmInsideElement(x, y){
+    const amplitude = this.sideLength / Math.sqrt(2);
+    const fortyFiveOffset = 45 * Math.PI / 180;
+    const angleRadians = this.angle * Math.PI / 180;
+
+    // Calculate position of Point A
+    const posA = [
+      this.posX - amplitude * Math.cos(angleRadians - fortyFiveOffset),
+      this.posY - amplitude * Math.cos(angleRadians + fortyFiveOffset)
+    ];
+
+    // Calculate position of Point B
+    const posB = [
+      this.posX - amplitude * Math.cos(angleRadians + fortyFiveOffset),
+      this.posY + amplitude * Math.cos(angleRadians - fortyFiveOffset)
+    ];
+
+    // Calculate position of Point D
+    const posD = [
+      this.posX + amplitude * Math.cos(angleRadians + fortyFiveOffset),
+      this.posY - amplitude * Math.cos(angleRadians - fortyFiveOffset)
+    ];
+
+    // Calculate vectors of AM, AB, and AD
+    const aToM = [x - posA[0], y - posA[1]]
+    const aToB = [posB[0] - posA[0], posB[1] - posA[1]];
+    const aToD = [posD[0] - posA[0], posD[1] - posA[1]];
+
+    function scalarProduct(vec1, vec2){
+      return vec1[0] * vec2[0] + vec1[1] * vec2[1];
+    }
+
+    // debugger;
+
+    // Confirm that AM * AB < AB squared and DM * AD < AD squared (which confirms that point is in circle)
+    return scalarProduct(aToM, aToB) < scalarProduct(aToB, aToB) && scalarProduct(aToM, aToD) < scalarProduct(aToD, aToD);
   }
 };
 
